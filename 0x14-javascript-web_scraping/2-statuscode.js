272bc9a8db83e808896aa7d008aa948f8eebba65
Izy4ezee <chibuezeokorieone@gmail.com>
2023-04-27 18:18:20 +0000

    done

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
+
