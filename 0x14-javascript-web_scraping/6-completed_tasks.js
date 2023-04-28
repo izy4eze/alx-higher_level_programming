e5bf25b1723d284e02f608ca1fcd67374cda76d0
Izy4ezee <chibuezeokorieone@gmail.com>
2023-04-28 18:59:53 +0000

    Task done

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,4 +1,6 @@
-t request = require('request');
+#!/usr/bin/node
+
+const request = require('request');
 const url = process.argv[2];
 
 request.get(url, { json: true }, (error, response, body) => {
